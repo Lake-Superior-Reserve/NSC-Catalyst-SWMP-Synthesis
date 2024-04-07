9f928b43a87708737da73c2ae186254f4169b328
Lindsay Platt <lrplatt@wisc.edu>
2024-04-06 21:26:54 -0500

    add wavelet transform

--- TimeSeriesAnalysis.R
+++ TimeSeriesAnalysis.R
@@ -74,3 +74,49 @@ decomposed_ts_all %>%
   facet_wrap(vars(station, param), scales='free', ncol=2) +
   theme_bw() +
   ggtitle('Seasonal signals')
+
+##### Wavelet analysis #####
+
+library(wsyn)
+library(zoo)
+
+# Borrowed these code concepts for using `wsyn` from Danny Szydlowski
+
+# Wilkinson, G. M., Walter, J., Fleck, R., & Pace, M. L. (2020). 
+# Beyond the trends: The need to understand multiannual dynamics in aquatic ecosystems. 
+# Limnology and Oceanography Letters, 5(4), 281–286. https://doi.org/10.1002/lol2.10153
+
+# Prepare data for calculating wavelets
+for(this_station in c('gb', 'sq')) {
+  swmp_wavelet_data <- swmp_data_prep %>%
+    filter(station %in% c(this_station, 'gb-gl')) %>% 
+    select(year, month, year_frac, param, value) %>% 
+    pivot_wider(id_cols = c(year, month, year_frac), names_from = 'param', values_from = 'value') %>% 
+    # Fill NAs for missing data
+    mutate(temp_median = zoo::na.approx(temp_median, na.rm = FALSE),
+           turb_median = zoo::na.approx(turb_median, na.rm = FALSE),
+           no23f = zoo::na.approx(no23f, na.rm = FALSE)) %>% 
+    # Filter out the first three values which cannot be linearly interpolated to fill NAs
+    filter(!is.na(temp_median) & !is.na(turb_median) & !is.na(no23f))
+  
+  # Clean the data to fit wavelet assumptions
+  no23f <- cleandat(swmp_wavelet_data$no23f, 1:length(swmp_wavelet_data$no23f), clev=5)$cdat
+  precip <- cleandat(swmp_wavelet_data$totprcp_total, 1:length(swmp_wavelet_data$totprcp_total), clev=5)$cdat
+  turb <- cleandat(swmp_wavelet_data$turb_median, 1:length(swmp_wavelet_data$turb_median), clev=5)$cdat
+  temp <- cleandat(swmp_wavelet_data$temp_median, 1:length(swmp_wavelet_data$temp_median), clev=5)$cdat
+  
+  # we can then apply the wavelet transform to break the data down into oscillating
+  # patterns at different timescales
+  wt.no23f <- wt(no23f, 1:length(no23f))
+  wt.precip <- wt(precip, 1:length(precip))
+  wt.turb <- wt(turb, 1:length(turb))
+  wt.temp <- wt(temp, 1:length(temp))
+  
+  # arrange them side by side
+  par(mfrow=c(1,4), mai=c(0.3,0.3,0.3,0.3), omi=c(0.4,0.4,0.2,0.1))
+  
+  plotmag(wt.precip, title = sprintf("Precip - %s", this_station))
+  plotmag(wt.no23f, title = sprintf("NO23f - %s", this_station))
+  plotmag(wt.turb, title = sprintf("Turbdity - %s", this_station))
+  plotmag(wt.temp, title = sprintf("Temp - %s", this_station))
+}
