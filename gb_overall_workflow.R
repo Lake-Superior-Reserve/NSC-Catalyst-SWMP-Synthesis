124a4440b26a5be4fcb55c80aea2a184b4930a95
Lindsay Platt <lrplatt@wisc.edu>
2024-04-20 19:04:10 -0500

    commit file with overall structure for code and analyses

---
+++ gb_overall_workflow.R
@@ -0,0 +1,35 @@
+# Final analysis script for Great Bay NERR
+# Lizzie Emch and Lindsay Platt
+# April 2024
+
+# Load library dependencies
+library(tidyverse)
+
+# Load local functions
+source()
+
+##### 1 Data loading / preparing #####
+
+##### 2 Data exploration and visual validation #####
+
+###### 2A Time series gaps ######
+
+###### 2B Site maps ######
+
+##### 3 Scales of variability #####
+
+###### 3A Time series decomposition ######
+
+###### 3B Seasonal signals via wavelet transform ######
+
+##### 4 Time series trend analysis #####
+
+###### 4A Mann-Kendall ######
+
+###### 4B Seasonal Mann-Kendall ######
+
+###### 4C Mann-Kendall by each season ######
+
+##### 5 Time series break-point analysis #####
+
+##### 6 Time series dynamics, linearity and non-linearity #####
